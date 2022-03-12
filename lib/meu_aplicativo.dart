12caaabcc235541a46b2d163e70a26a9ecd16af1
ygamachado <72772753+ygamachado@users.noreply.github.com>
2022-03-11 22:26:20 -0300

    connect to firestore

--- lib/meu_aplicativo.dart
+++ lib/meu_aplicativo.dart
@@ -1,5 +1,7 @@
 import 'package:busca_facil/pages/home_page.dart';
 import 'package:busca_facil/widgets/auth_check.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class MeuAplicativo extends StatelessWidget {
@@ -7,6 +9,13 @@ class MeuAplicativo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FirebaseFirestore.instance.collection('todo').get().then((value) {
+      value.docs.forEach((element) {
+        print(element.data());
+      });
+    });
+
+
     return MaterialApp(
       title: 'Busca Facil',
       debugShowCheckedModeBanner: false,
